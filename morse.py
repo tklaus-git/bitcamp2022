f3ced6237e2a29ec7279e69af3428495984c4f7e
tklaus-git <46978669+tklaus-git@users.noreply.github.com>
2022-04-09 19:40:09 -0400

    Working on blink

--- morse.py
+++ morse.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import cvzone
 from cvzone.FaceMeshModule import FaceMeshDetector
-import numpy as np
+from cvzone.PlotModule import LivePlot
 import mediapipe
 
 video = cv2.VideoCapture(1)
@@ -10,6 +10,10 @@ if (video.isOpened() == False):
   print("Error opening video stream or file")
 
 detector = FaceMeshDetector(maxFaces=1)
+
+plotY= LivePlot(640, 360, [20, 50])
+
+
 idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
 
 while True:
@@ -23,8 +27,19 @@ while True:
             cv2.circle(img, face[id], 5, (255, 0, 255), cv2.FILLED)
         leftUp = face[159]
         leftDown = face[23]
-        distanceHor, _ = detector.findDistance(leftUp, leftDown)
-        print(distanceHor)
+
+        leftLeft = face[130]
+        leftRight = face[243]
+
+        distanceVert, _ = detector.findDistance(leftUp, leftDown)
+        distanceHor, _ = detector.findDistance(leftLeft, leftRight)
+
+        distanceVert = (distanceVert/distanceHor)*100
+
+        print(int(distanceVert))
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+
         if distanceHor<12:
             print('\nBLINK\n')
 
