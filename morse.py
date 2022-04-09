7b17cb889bc937e9df589c4d1a3761779b3bc6d0
raeenf <43077415+raeenf@users.noreply.github.com>
2022-04-09 12:24:54 -0400

    video success

--- morse.py
+++ morse.py
@@ -1,2 +1,12 @@
 import face_recognition
+import cv2
+import numpy as np
 
+video = cv2.VideoCapture(0)
+if (video.isOpened() == False):
+  print("Error opening video stream or file")
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
