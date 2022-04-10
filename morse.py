9846cf7df510afb9a564806d40db576068904aef
tklaus-git <46978669+tklaus-git@users.noreply.github.com>
2022-04-10 11:20:11 -0400

    added a few comments and removed prints

--- morse.py
+++ morse.py
@@ -7,14 +7,13 @@ from cvzone.PlotModule import LivePlot
 import mediapipe
 import time
 import keyboard
-from typing import List
 
 #           Binary Tree representation
 letters = ['','E','T','I','A','N','M','S','U','R','W','D','K','G','O','H','V','F','','L','','P','J','B','X','C','Y','Z','Q','','']
 
 
 # dots is a list of 0's and 1's; 0: short blink, 1: long blink
-def choose_letter(dots: List[int]) -> str:
+def choose_letter(dots):
     if len(dots)>4:
         return 'TOO BIG'
     trace = 1
@@ -23,9 +22,12 @@ def choose_letter(dots: List[int]) -> str:
             trace = 2*trace
         else:
             trace = (2*trace)+1
-    print(trace-1)
+ 
         
     return letters[int(trace-1)]
+
+
+
 
 
 video = cv2.VideoCapture(1) # Opening my front camera 
@@ -49,14 +51,12 @@ blinking = 0 # are we blinking
 timer = time.perf_counter()
 notBlinking = 0
 
-avg_d = [] # Will keep track of the average ratio between distances within eye
+avg_d = []
 
-letter = []
-word = ''
+letter = [] # keeps track of dashes and dots
+word = '' # all letters "typed"
 morse = ''
 start = False
-
-
 while not keyboard.is_pressed('q'):
     success, img = video.read()
     img, faces = detector.findFaceMesh(img, draw = False) # changed
@@ -64,10 +64,7 @@ while not keyboard.is_pressed('q'):
     if keyboard.is_pressed('s'):
         start = True   
 
-    
-
     if faces: # if face is detected
-        # Setting up variables related to positions
         face = faces[0]
         leftUp = face[159]
         leftDown = face[23]
@@ -87,46 +84,35 @@ while not keyboard.is_pressed('q'):
         distanceHor, _ = detector.findDistance(leftLeft, leftRight)
         distanceVertR, _ = detector.findDistance(rightUp, rightDown)
         distanceHorR, _ = detector.findDistance(rightLeft, rightRight)
-
-        # if (distanceVert >= 12 and distanceHor >= 30):
-
-
-        # print('\nVert: '+str(distanceVert))
-        # print('Hor: '+str(distanceHor))
         
-    
-
+        # drawing points in left eye
         for id in idList:
             cv2.circle(img, face[id], 5, (255, 0, 255)) # just here to color eye
         
-        # Getting the 4 points we will keep track of, the 4 corners of the eyes
-        
+        # Getting the 4 points we will keep track of, the 4 corners of the eyes:
         horList.append((distanceHor+distanceHorR)/2)
         if len(horList) > 1:
             horList.pop(0)
         else:
             blinking = 0
-        distanceHor = sum(horList)/len(horList) # Average horizontal distance past 100 frames
+        distanceHor = sum(horList)/len(horList) # Average horizontal distance past 1 frames
 
         dList.append((distanceVert+distanceVertR)/2)
         if len(dList) > 2:
             dList.pop(0)
-        distanceVert = sum(dList)/len(dList)
+        distanceVert = sum(dList)/len(dList) # average vertical distance for the past 2 frames
 
-        ratio = (distanceVert/distanceHor)*100
+        ratio = (distanceVert/distanceHor)*100 # ratio of vertical and horizontal distance
         r_list.append(ratio)
         if len(r_list)>2:
             r_list.pop(0)
         ratio = sum(r_list)/len(r_list)
         
         # Blinking detection:
-
         avg_d.append(ratio)
         if len(avg_d) > 200:
             avg_d.pop(0)
         avg_ratio =   sum(avg_d)/len(avg_d)
-        print(avg_ratio-ratio)
-
 
         if start:
             if avg_ratio-ratio > 3: 
@@ -134,6 +120,7 @@ while not keyboard.is_pressed('q'):
                     startTime = time.perf_counter()
                     blinking = 1
                     counter+=1  
+
             else: # Wasn't blinking
                 if blinking == 1: # end of blink
                     howLong = time.perf_counter() - startTime
@@ -151,15 +138,9 @@ while not keyboard.is_pressed('q'):
                     letter = []
                     morse = ''
 
-
-                # IF IT IS A DASH OR A DOT
-
-
                 blinking = 0 # not blinking
-            # print(counter)
             
         cvzone.putTextRect(img, word+morse, (100,100))
-        # cvzone.putTextRect(img, morse, (100,200))
 
         # Plotting
         
@@ -169,7 +150,6 @@ while not keyboard.is_pressed('q'):
         cv2.line(img, leftUp, leftDown, (0, 200, 0), 1)
         cv2.line(img, leftUp, leftDown, (0, 200, 0), 1)
         cv2.imshow('ImagePlot', imgPlot)
-        # print(round(time.perf_counter(),2))
         
         if keyboard.is_pressed('r'):
             letter = []
