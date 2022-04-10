e9df4b6aaa2a71b21fa8ec6586554dcd4976c10e
tklaus-git <46978669+tklaus-git@users.noreply.github.com>
2022-04-10 09:49:44 -0400

    Final changes, no comments

--- morse.py
+++ morse.py
@@ -6,6 +6,7 @@ from cvzone.FaceMeshModule import FaceMeshDetector
 from cvzone.PlotModule import LivePlot
 import mediapipe
 import time
+import keyboard
 
 #           Binary Tree representation
 letters = ['','E','T','I','A','N','M','S','U','R','W','D','K','G','O','H','V','F','','L','','P','J','B','X','C','Y','Z','Q','','']
@@ -44,6 +45,7 @@ plotY= LivePlot(640, 360, [20, 50])
 
 idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243] # Specific points I am keeping track of 
 
+
 dList = [] # Going to keep track of distances between top and bottom of eye
 horList = [] # Going to keep track of distance between left and right side of eye
 r_list = []
@@ -56,11 +58,14 @@ notBlinking = 0
 letter = []
 word = ''
 morse = ''
-
-while True:
+start = False
+while not keyboard.is_pressed('q'):
     success, img = video.read()
-    img, faces = detector.findFaceMesh(img, draw=False)
-    
+    img, faces = detector.findFaceMesh(img, draw = False) # changed
+
+    if keyboard.is_pressed('s'):
+        start = True   
+
     
 
     if faces: # if face is detected
@@ -71,10 +76,18 @@ while True:
         leftLeft = face[130]
         leftRight = face[243]
 
+        rightDown = face[374]
+        rightUp = face[386]
+
+        rightRight = face[359]
+        rightLeft = face[398]
+
 
         # Getting distance
         distanceVert, _ = detector.findDistance(leftUp, leftDown)
         distanceHor, _ = detector.findDistance(leftLeft, leftRight)
+        distanceVertR, _ = detector.findDistance(rightUp, rightDown)
+        distanceHorR, _ = detector.findDistance(rightLeft, rightRight)
 
         # if (distanceVert >= 12 and distanceHor >= 30):
 
@@ -89,55 +102,55 @@ while True:
         
         # Getting the 4 points we will keep track of, the 4 corners of the eyes
         
-        # horList.append(distanceHor)
-        # if len(horList) > 2:
-        #     horList.pop(0)
-        # else:
-        #     blinking = 0
-        # distanceHor = sum(horList)/len(horList) # Average horizontal distance past 100 frames
+        horList.append((distanceHor+distanceHorR)/2)
+        if len(horList) > 1:
+            horList.pop(0)
+        else:
+            blinking = 0
+        distanceHor = sum(horList)/len(horList) # Average horizontal distance past 100 frames
 
-        # dList.append(distanceVert)
-        # if len(dList) > 2:
-        #     dList.pop(0)
-        # distanceVert = sum(dList)/len(dList)
+        dList.append((distanceVert+distanceVertR)/2)
+        if len(dList) > 2:
+            dList.pop(0)
+        distanceVert = sum(dList)/len(dList)
 
         ratio = (distanceVert/distanceHor)*100
         r_list.append(ratio)
         if len(r_list)>2:
             r_list.pop(0)
         ratio = sum(r_list)/len(r_list)
-
+        print(ratio)
         # Blinking detection:
-        if ratio < 30: 
-            if blinking == 0:
-                startTime = time.perf_counter()
-                blinking = 1
-                counter+=1  
-        else: # Wasn't blinking
-            if blinking == 1: # end of blink
-                howLong = time.perf_counter() - startTime
-                notBlinking = time.perf_counter()
-                print(howLong)
-                if (howLong<.2): # dot
-                    letter.append(0)
-                    morse+= '.'
-                elif(howLong < 1):
-                    letter.append(1)
-                    morse+= '_'                    
-            if time.perf_counter() - notBlinking >= 1:
-                # LETTER IS COMPLETE:
-                if len(letter) < 5:
-                    word += choose_letter(letter)
-                letter = []
-                morse = ''
+        if start:
+            if ratio < 27: 
+                if blinking == 0:
+                    startTime = time.perf_counter()
+                    blinking = 1
+                    counter+=1  
+            else: # Wasn't blinking
+                if blinking == 1: # end of blink
+                    howLong = time.perf_counter() - startTime
+                    notBlinking = time.perf_counter()
+                    if (howLong<.27): # dot
+                        letter.append(0)
+                        morse+= '.'
+                    elif(howLong < 1):
+                        letter.append(1)
+                        morse+= '_'                    
+                if time.perf_counter() - notBlinking >= 1:
+                    # LETTER IS COMPLETE:
+                    if len(letter) < 5:
+                        word += choose_letter(letter)
+                    letter = []
+                    morse = ''
 
 
-            # IF IT IS A DASH OR A DOT
+                # IF IT IS A DASH OR A DOT
 
 
-            blinking = 0 # not blinking
-        # print(counter)
-        
+                blinking = 0 # not blinking
+            # print(counter)
+            
         cvzone.putTextRect(img, word+morse, (100,100))
         # cvzone.putTextRect(img, morse, (100,200))
 
@@ -151,8 +164,12 @@ while True:
         cv2.imshow('ImagePlot', imgPlot)
         # print(round(time.perf_counter(),2))
         
-    # print(distanceVert)
+        if keyboard.is_pressed('r'):
+            letter = []
+            morse = ''
+            word = ''
+
+
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
-
