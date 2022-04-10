c33fa8af92f825db84673e1aacfdb63355469543
raeenf <43077415+raeenf@users.noreply.github.com>
2022-04-09 20:03:31 -0400

    fixed letter function

--- morse.py
+++ morse.py
@@ -47,8 +47,11 @@ while True:
     cv2.waitKey(1)
 
 
+
+
 #           1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
-letters = ['','E','T','I','A','N','M','S','U','R','W','D','K','G','O','H','V','F','L','P','J','B','X','C','Y','Z','Q']
+letters = ['','E','T','I','A','N','M','S','U','R','W','D','K','G','O','H','V','F','','L','','P','J','B','X','C','Y','Z','Q','','']
+dots = [0,0,1,1]
 
 # dots is a list of 0's and 1's; 0: short blink, 1: long blink
 def letter(dots):
@@ -58,4 +61,12 @@ def letter(dots):
             trace = 2*trace
         else:
             trace = (2*trace)+1
-    return letters[trace-1]
+            
+    if letters[trace-1] == '' and trace%2 == 1:
+        trace = (trace-1)/2
+    elif letters[trace-1] == '' and trace%2 == 0:
+        trace = trace/2
+        
+    return letters[int(trace-1)]
+
+print(letter(dots))
