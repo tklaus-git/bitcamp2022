fddcb69a0a876a482dbb7a1bd74eafd5dae11e61
tklaus-git <46978669+tklaus-git@users.noreply.github.com>
2022-04-10 02:25:18 -0400

    FINISHED- no comments

--- morse.py
+++ morse.py
@@ -1,72 +1,158 @@
+from time import perf_counter
 import numpy as np
 import cv2
 import cvzone
 from cvzone.FaceMeshModule import FaceMeshDetector
 from cvzone.PlotModule import LivePlot
 import mediapipe
+import time
 
-video = cv2.VideoCapture(1)
-if (video.isOpened() == False):
-  print("Error opening video stream or file")
-
-detector = FaceMeshDetector(maxFaces=1)
-
-plotY= LivePlot(640, 360, [20, 50])
-
-
-idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
-
-while True:
-
-    success, img = video.read()
-    img, faces = detector.findFaceMesh(img, draw=False)
-
-    if faces:
-        face = faces[0]
-        for id in idList:
-            cv2.circle(img, face[id], 5, (255, 0, 255), cv2.FILLED)
-        leftUp = face[159]
-        leftDown = face[23]
-
-        leftLeft = face[130]
-        leftRight = face[243]
-
-        distanceVert, _ = detector.findDistance(leftUp, leftDown)
-        distanceHor, _ = detector.findDistance(leftLeft, leftRight)
-
-        distanceVert = (distanceVert/distanceHor)*100
-
-        print(int(distanceVert))
-        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
-        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
-
-        if distanceHor<12:
-            print('\nBLINK\n')
-
-    cv2.imshow('Image', img)
-    cv2.waitKey(1)
-
-
-
-
-#           1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
+#           Binary Tree representation
 letters = ['','E','T','I','A','N','M','S','U','R','W','D','K','G','O','H','V','F','','L','','P','J','B','X','C','Y','Z','Q','','']
-dots = [0,0,1,1]
+
 
 # dots is a list of 0's and 1's; 0: short blink, 1: long blink
-def letter(dots):
+def choose_letter(dots):
+    if len(dots)>4:
+        return 'TOO BIG'
     trace = 1
     for dot in dots:
         if dot == 0:
             trace = 2*trace
         else:
             trace = (2*trace)+1
-            
-    if letters[trace-1] == '' and trace%2 == 1:
-        trace = (trace-1)/2
-    elif letters[trace-1] == '' and trace%2 == 0:
-        trace = trace/2
+    print(trace-1)
+    #if letters[trace-1] == '' and trace%2 == 1:
+     #   trace = (trace-1)/2
+    #elif letters[trace-1] == '' and trace%2 == 0:
+     #   trace = trace/2
         
     return letters[int(trace-1)]
 
-print(letter(dots))
+
+
+
+
+video = cv2.VideoCapture(1) # Opening my front camera 
+if (video.isOpened() == False):
+  print("Error opening video stream or file")
+
+detector = FaceMeshDetector(maxFaces=1) # Find face mesh, keeps track of points in face
+
+plotY= LivePlot(640, 360, [20, 50])
+
+
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243] # Specific points I am keeping track of 
+
+dList = [] # Going to keep track of distances between top and bottom of eye
+horList = [] # Going to keep track of distance between left and right side of eye
+r_list = []
+
+counter = 0 # Keeps track of what blink we are on
+blinking = 0 # are we blinking
+timer = time.perf_counter()
+notBlinking = 0
+
+letter = []
+word = ''
+morse = ''
+
+while True:
+    success, img = video.read()
+    img, faces = detector.findFaceMesh(img, draw=False)
+    
+    
+
+    if faces: # if face is detected
+        face = faces[0]
+        leftUp = face[159]
+        leftDown = face[23]
+
+        leftLeft = face[130]
+        leftRight = face[243]
+
+
+        # Getting distance
+        distanceVert, _ = detector.findDistance(leftUp, leftDown)
+        distanceHor, _ = detector.findDistance(leftLeft, leftRight)
+
+        # if (distanceVert >= 12 and distanceHor >= 30):
+
+
+        # print('\nVert: '+str(distanceVert))
+        # print('Hor: '+str(distanceHor))
+        
+    
+
+        for id in idList:
+            cv2.circle(img, face[id], 5, (255, 0, 255)) # just here to color eye
+        
+        # Getting the 4 points we will keep track of, the 4 corners of the eyes
+        
+        # horList.append(distanceHor)
+        # if len(horList) > 2:
+        #     horList.pop(0)
+        # else:
+        #     blinking = 0
+        # distanceHor = sum(horList)/len(horList) # Average horizontal distance past 100 frames
+
+        # dList.append(distanceVert)
+        # if len(dList) > 2:
+        #     dList.pop(0)
+        # distanceVert = sum(dList)/len(dList)
+
+        ratio = (distanceVert/distanceHor)*100
+        r_list.append(ratio)
+        if len(r_list)>2:
+            r_list.pop(0)
+        ratio = sum(r_list)/len(r_list)
+
+        # Blinking detection:
+        if ratio < 30: 
+            if blinking == 0:
+                startTime = time.perf_counter()
+                blinking = 1
+                counter+=1  
+        else: # Wasn't blinking
+            if blinking == 1: # end of blink
+                howLong = time.perf_counter() - startTime
+                notBlinking = time.perf_counter()
+                print(howLong)
+                if (howLong<.2): # dot
+                    letter.append(0)
+                    morse+= '.'
+                elif(howLong < 1):
+                    letter.append(1)
+                    morse+= '_'                    
+            if time.perf_counter() - notBlinking >= 1:
+                # LETTER IS COMPLETE:
+                if len(letter) < 5:
+                    word += choose_letter(letter)
+                letter = []
+                morse = ''
+
+
+            # IF IT IS A DASH OR A DOT
+
+
+            blinking = 0 # not blinking
+        # print(counter)
+        
+        cvzone.putTextRect(img, word+morse, (100,100))
+        # cvzone.putTextRect(img, morse, (100,200))
+
+        # Plotting
+        
+        imgPlot = plotY.update(ratio)
+
+
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 1)
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 1)
+        cv2.imshow('ImagePlot', imgPlot)
+        # print(round(time.perf_counter(),2))
+        
+    # print(distanceVert)
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
+
